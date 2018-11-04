72523cbba0b0ef0b9a5668df7abfc51609d04386
lucasbortolini0 <37717526+lucasbortolini0@users.noreply.github.com>
2018-11-04 20:12:15 -0200

    Add files via upload
    tentei terminar o codigo

---
+++ Cartas-master/src/Player.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Player {
+	public static final int TAMANHO_MAO = 3;
+    
+    private Carta[] mao;
+	private int pontuacao;
+	private int pontuacaoRodada;
+	private int pontoMao;
+	
+	public Jogador() {
+		mao = new Carta[TAMANHO_MAO];
+		pontuacao = 0;
+		pontoMao = 0;
+	}
+
+	/// Inacabado o resto
+	
+}
---
+++ Cartas-master/src/TestaTruco.java
@@ -0,0 +1,10 @@
+public class TestaTruco {
+
+	public static void main(String[] args) {
+		Truco jogo = new Truco();
+		
+		jogo.jogar();
+
+	}
+
+}
